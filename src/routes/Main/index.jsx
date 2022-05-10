513602e2e5205359721abf6aedbb12817a0e3720
keunyeong <91kyeong@gmail.com>
2022-05-10 11:55:56 +0900

    Feat: Initial Setting Page.

---
+++ src/routes/Main/index.jsx
@@ -0,0 +1,7 @@
+import styles from './Main.module.scss'
+
+const Main = () => {
+  return <main className={styles.main}>영화 검색 사이트 시작</main>
+}
+
+export default Main
--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,20 +1,15 @@
 import styles from './Routes.module.scss'
 import { Routes, Route } from 'react-router-dom'
-import TodoList from './TodoList'
-import Weather from './Weathers'
-import GNB from 'routes/_shared/GNB'
+// import GNB from 'routes/_shared/GNB'
+import Main from './Main'
 
 const App = () => {
   return (
     <div className={styles.appWrapper}>
-      <GNB />
+      {/* <GNB /> */}
       <div className={styles.app}>
         <Routes>
-          <Route path='/' element={<TodoList />} />
-          <Route path='todo' element={<TodoList />} />
-          <Route path='weather' element={<Weather />}>
-            <Route path=':city' element={<Weather />} />
-          </Route>
+          <Route path='/' element={<Main />} />
         </Routes>
       </div>
     </div>
