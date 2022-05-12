54c84fc419436489fb176ca4b9a5bc80b5e501aa
keunyeong <91kyeong@gmail.com>
2022-05-12 15:48:49 +0900

    Feat: Footer NavLink 연결

--- src/components/footer/index.jsx
+++ src/components/footer/index.jsx
@@ -1,8 +1,26 @@
 import styles from './footer.module.scss'
 import 'animate.css'
+import { NavLink } from 'react-router-dom'
+import cx from 'classnames'
+import { SearchIcon, StarIcon } from '../../assets/svgs'
 
 const Footer = () => {
-  return <footer className={styles.footer} />
+  return (
+    <footer className={styles.footer}>
+      <div>
+        <NavLink to='/' className={cx(styles.nav)}>
+          <SearchIcon />
+          SEARCH
+        </NavLink>
+      </div>
+      <div>
+        <NavLink to='favorites' className={cx(styles.nav)}>
+          <StarIcon />
+          PICKS
+        </NavLink>
+      </div>
+    </footer>
+  )
 }
 
 export default Footer
